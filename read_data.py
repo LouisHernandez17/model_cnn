60c2e8aaf4155f9dd2cf7a3d607c6929c31a6ec0
Louis <louis.hernandez35.lh@gmail.com>
2021-06-29 18:21:42 +0200

    Trained on more data comparison

--- read_data.py
+++ read_data.py
@@ -10,12 +10,6 @@ def read_bag(path):
     for f in os.listdir(path):
         if f[-4:]=='.bag':
             bag=bagpy.bagreader(os.path.join(path,f))
-        elif num<21:#No Noise
-            label=[1,0,0]
-        elif num<41:#Scan Noise
-            label=[0,1,0]
-        elif num<61:#Odom Noise
-            label=[0,0,1]
     odom=bag.odometry_data()
     scan=bag.message_by_topic("/scan")
     #Opens as Dataframes
@@ -26,17 +20,24 @@ def read_bag(path):
     df_od=df_od[od_cols].fillna(method="ffill").fillna(value=0)
     sc_cols=[i for i in df_sc.columns if i.split('_')[0]=='intensities']
     df_sc=df_sc[sc_cols].fillna(method="ffill").fillna(value=0)
-    return(df_od,df_sc,label)
+    return(df_od,df_sc)
     
 # %%
 def make_dataset(path):
     odoms=[]
     scans=[]
     labels=[]
-    for folder in os.listdir(path):
-        df_od,df_sc,label=read_bag(os.path.join(path,folder))
-        odoms.append(df_od.values)
-        scans.append(df_sc.values)
-        labels.append(label)
+    for folder_class in os.listdir(path):
+        if folder_class=='NoNoise':
+            label=[1,0,0]
+        elif folder_class=='OdomNoise':
+            label=[0,1,0]
+        elif folder_class=='ScanNoise':
+            label=[0,0,1]
+        for folder_data in os.listdir(os.path.join(path,folder_class)):
+            df_od,df_sc=read_bag(os.path.join(path,folder_class,folder_data))
+            odoms.append(df_od.values)
+            scans.append(df_sc.values)
+            labels.append(label)
     return(odoms,scans,labels)
 # %%
