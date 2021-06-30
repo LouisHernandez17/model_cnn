088bc20eddde74599da59c6e9de205b9c7fd51f0
Louis <louis.hernandez35.lh@gmail.com>
2021-06-30 16:58:41 +0200

    Changed input Pipeline + Added best model saving

--- cnn.py
+++ cnn.py
@@ -8,9 +8,9 @@ import random
 class Turtlebot_CNN(tf.keras.Model):
     def __init__(self,type="cnn"):
         super(Turtlebot_CNN,self).__init__()
-        self.input_od=tf.keras.layers.InputLayer(input_shape=(None,None,9))
-        self.input_sc=tf.keras.layers.InputLayer(input_shape=(None,None,360))
         self.type=type
+        self.input_Od=tf.keras.layers.InputLayer(input_shape=(None,None,13),batch_size=1)
+        self.input_Sc=tf.keras.layers.InputLayer(input_shape=(None,None,360),batch_size=1)
         if type=="cnn":
             self.Od1=tf.keras.layers.Conv1D(filters=64,kernel_size=10,activation=tf.nn.relu)
             self.Od2=tf.keras.layers.Conv1D(filters=32,kernel_size=10,activation=tf.nn.relu)
@@ -31,15 +31,13 @@ class Turtlebot_CNN(tf.keras.Model):
         self.dense2=tf.keras.layers.Dense(128,activation=tf.nn.relu)
         self.dense3=tf.keras.layers.Dense(3,activation=tf.nn.softmax)
     def call(self,inputs):
-        x1=inputs[0]
-        x2=inputs[1]
-        x1=self.input_od(x1)
+        x1,x2=inputs
         x1=self.Od1(x1)
         x1=self.Od2(x1)
-        x2=self.input_sc(x2)
         x2=self.Sc1(x2)
         x2=self.Sc2(x2)
         x2=self.Sc3(x2)
+        print(x2)
         if self.type=='cnn':
             x2=self.avg_pool_sc(x2)
             x1=self.avg_pool_od(x1)
@@ -49,35 +47,21 @@ class Turtlebot_CNN(tf.keras.Model):
         return(self.dense3(x))
     
 # %%
-class Sequence_1by1(tf.keras.utils.Sequence):
-    def __init__(self,x,y):
-        self.x,self.y=x,y
-        self.batch_size=1
-    def __len__(self):
-        return math.ceil(len(self.x[0]) / self.batch_size)
-
-    def __getitem__(self, idx):
-        batch_x1 = self.x[0][idx * self.batch_size:(idx + 1) *
-        self.batch_size]
-        batch_x2 = self.x[1][idx * self.batch_size:(idx + 1) *
-        self.batch_size]
-        batch_y = self.y[idx * self.batch_size:(idx + 1) *
-        self.batch_size]
-
-        return [np.array(batch_x1),np.array(batch_x2)], np.array(batch_y)
-
-
 def data_preparation(path,training=0.7):
     odoms,scans,labels=make_dataset(path)
-    l=list(zip(odoms,scans,labels))
-    random.shuffle(l)
-    i=math.ceil((len(l)-1)*training)
-    train_list=l[:i]
-    test_list=l[i:]
-    odoms_train,scans_train,labels_train=zip(*train_list)
-    odoms_test,scans_test,labels_test=zip(*test_list)
-    train=Sequence_1by1([odoms_train,scans_train],labels_train)
-    test=Sequence_1by1([odoms_test,scans_test],labels_test)
+    i=math.ceil((len(odoms)-1)*training)
+    odoms_train,scans_train,labels_train=odoms[:i],scans[:i],labels[:i]
+    odoms_test,scans_test,labels_test=odoms[i:],scans[i:],labels[i:]
+    odoms_train_ds=tf.data.Dataset.from_generator(lambda:odoms_train,tf.float64,output_shapes=(None,13))
+    scans_train_ds=tf.data.Dataset.from_generator(lambda:scans_train,tf.float64,output_shapes=(None,360))
+    labels_train_ds=tf.data.Dataset.from_generator(lambda:labels_train,tf.float64)
+    odoms_test_ds=tf.data.Dataset.from_generator(lambda:odoms_test,tf.float64,output_shapes=(None,13))
+    scans_test_ds=tf.data.Dataset.from_generator(lambda:scans_test,tf.float64,output_shapes=(None,630))
+    labels_test_ds=tf.data.Dataset.from_generator(lambda:labels_test,tf.float64)
+    X_train=tf.data.Dataset.zip((odoms_train_ds,scans_train_ds))
+    X_test=tf.data.Dataset.zip((odoms_test_ds,scans_test_ds))
+    train=tf.data.Dataset.zip((X_train,labels_train_ds)).batch(1)
+    test=tf.data.Dataset.zip((X_test,labels_test_ds)).batch(1)
     return train,test
     
 
