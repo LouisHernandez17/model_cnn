b3c5e97920a92601517d915a26a7382c8bf79f3a
LouisHernandez17 <louis.hernandez35.lh@gmail.com>
2021-07-06 10:22:56 +0200

    500Epochs training+Lightweight LSTM

--- cnn.py
+++ cnn.py
@@ -6,22 +6,14 @@ import math
 import random
 # %%
 class Turtlebot_CNN(tf.keras.Model):
-    def __init__(self,type="cnn"):
+    def __init__(self):
         super(Turtlebot_CNN,self).__init__()
         self.type=type
-        if type=="cnn":
-            self.Od1=tf.keras.layers.Conv1D(filters=64,kernel_size=10)
-            self.Od2=tf.keras.layers.Conv1D(filters=32,kernel_size=10)
-            self.Sc1=tf.keras.layers.Conv1D(filters=128,kernel_size=10)
-            self.Sc2=tf.keras.layers.Conv1D(filters=64,kernel_size=10)
-            self.Sc3=tf.keras.layers.Conv1D(filters=32,kernel_size=10)
-        elif type=="lstm":
-            self.Od1=tf.keras.layers.LSTM(64,return_sequences=True)
-            self.Od2=tf.keras.layers.LSTM(32)
-            self.Sc1=tf.keras.layers.LSTM(128,return_sequences=True)
-            self.Sc2=tf.keras.layers.LSTM(64,return_sequences=True)
-            self.Sc3=tf.keras.layers.LSTM(32)
-
+        self.Od1=tf.keras.layers.Conv1D(filters=64,kernel_size=10,padding="same")
+        self.Od2=tf.keras.layers.Conv1D(filters=32,kernel_size=10,padding="same")
+        self.Sc1=tf.keras.layers.Conv1D(filters=128,kernel_size=10,padding="same")
+        self.Sc2=tf.keras.layers.Conv1D(filters=64,kernel_size=10,padding="same")
+        self.Sc3=tf.keras.layers.Conv1D(filters=32,kernel_size=10,padding="same")
         self.avg_pool_od=tf.keras.layers.GlobalAveragePooling1D()
         self.avg_pool_sc=tf.keras.layers.GlobalAveragePooling1D()
         self.concat=tf.keras.layers.Concatenate()
@@ -35,9 +27,26 @@ class Turtlebot_CNN(tf.keras.Model):
         x2=self.Sc1(x2)
         x2=self.Sc2(x2)
         x2=self.Sc3(x2)
-        if self.type=='cnn':
-            x2=self.avg_pool_sc(x2)
-            x1=self.avg_pool_od(x1)
+        x2=self.avg_pool_sc(x2)
+        x1=self.avg_pool_od(x1)
+        x=self.concat([x1,x2])
+        x=self.dense1(x)
+        x=self.dense2(x)
+        return(self.dense3(x))
+class Turtlebot_LSTM(tf.keras.Model):
+    def __init__(self):
+        super(Turtlebot_LSTM,self).__init__()
+        self.type=type
+        self.Od=tf.keras.layers.LSTM(32)
+        self.Sc=tf.keras.layers.LSTM(32)
+        self.concat=tf.keras.layers.Concatenate()
+        self.dense1=tf.keras.layers.Dense(64,activation=tf.nn.relu)
+        self.dense2=tf.keras.layers.Dense(128,activation=tf.nn.relu)
+        self.dense3=tf.keras.layers.Dense(3,activation=tf.nn.softmax)
+    def call(self,inputs):
+        x1,x2=inputs
+        x1=self.Od(x1)
+        x2=self.Sc(x2)
         x=self.concat([x1,x2])
         x=self.dense1(x)
         x=self.dense2(x)
@@ -49,9 +58,9 @@ def data_preparation(path):
     l=len(odoms)
     odoms_ds=tf.data.Dataset.from_generator(lambda:odoms,tf.float64,output_shapes=(None,13))
     scans_ds=tf.data.Dataset.from_generator(lambda:scans,tf.float64,output_shapes=(None,360))
-    labels_ds=tf.data.Dataset.from_generator(lambda:labels,tf.float64)
+    labels_ds=tf.data.Dataset.from_generator(lambda:labels,tf.float64,output_shapes=(3,))
     X=tf.data.Dataset.zip((odoms_ds,scans_ds))
-    ds=tf.data.Dataset.zip((X,labels_ds)).batch(1)
+    ds=tf.data.Dataset.zip((X,labels_ds))
     return ds,l
     
 
