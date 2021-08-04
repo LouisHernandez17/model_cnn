d6f1d7bbc7f76933c88f79dec80b2f27016e97e0
LouisHernandez17 <louis.hernandez35.lh@gmail.com>
2021-08-04 10:06:53 +0200

    Added number of branches as argument for all models

--- lstm.py
+++ lstm.py
@@ -2,20 +2,20 @@
 import tensorflow as tf
 
 class Turtlebot_LSTM(tf.keras.Model):
-    def __init__(self):
+    def __init__(self,n_branches=2):
         super(Turtlebot_LSTM,self).__init__()
-        self.Od=tf.keras.layers.LSTM(32)
-        self.Sc=tf.keras.layers.LSTM(32)
+        self.n_branches=n_branches
+        self.lstms=[tf.keras.layers.LSTM(32) for i in range(self.n_branches)]
         self.concat=tf.keras.layers.Concatenate()
         self.dense1=tf.keras.layers.Dense(64,activation=tf.nn.relu)
         self.dense2=tf.keras.layers.Dense(128,activation=tf.nn.relu)
         self.dense3=tf.keras.layers.Dense(3,activation=tf.nn.softmax)
         self.short_name='lstm'
     def call(self,inputs):
-        x1,x2=inputs
-        x1=self.Od(x1)
-        x2=self.Sc(x2)
-        x=self.concat([x1,x2])
+        xs=[]
+        for i,x in enumerate(inputs):
+            xs.append(self.lstms[i](x))
+        x=self.concat(xs)
         x=self.dense1(x)
         x=self.dense2(x)
         return(self.dense3(x))
